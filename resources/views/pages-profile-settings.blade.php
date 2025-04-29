fd25b59b8805487ca3f84c3ce810b75d3088384f
john <johnjr092@gmail.com>
2025-04-29 09:20:06 +0300

    ui update

---
+++ resources/views/pages-profile-settings.blade.php
@@ -0,0 +1,140 @@
+@extends('layouts.master')
+@section('title')
+    @lang('translation.settings')
+@endsection
+@section('content')
+    <div class="row">
+        <div class="col-xxl-9">
+            <div class="card mt-xxl-n5">
+                <div class="card-header">
+                    <ul class="nav nav-tabs-custom rounded card-header-tabs border-bottom-0" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" data-bs-toggle="tab" href="#personalDetails" role="tab">
+                                <i class="fas fa-home"></i>
+                                Personal Details
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="tab" href="#changePassword" role="tab">
+                                <i class="far fa-user"></i>
+                                Change Password
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card-body p-4">
+                    <div class="tab-content">
+                        <div class="tab-pane active" id="personalDetails" role="tabpanel">
+                            <form action="javascript:void(0);">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="mb-3">
+                                            <label for="firstnameInput" class="form-label">First
+                                                Name</label>
+                                            <input type="text" class="form-control" id="firstnameInput"
+                                                placeholder="Enter your firstname" value="Dave">
+                                        </div>
+                                    </div>
+                                    <!--end col-->
+                                    <div class="col-lg-6">
+                                        <div class="mb-3">
+                                            <label for="lastnameInput" class="form-label">Last
+                                                Name</label>
+                                            <input type="text" class="form-control" id="lastnameInput"
+                                                placeholder="Enter your lastname" value="Adame">
+                                        </div>
+                                    </div>
+                                    <!--end col-->
+                                    <div class="col-lg-6">
+                                        <div class="mb-3">
+                                            <label for="phonenumberInput" class="form-label">Phone
+                                                Number</label>
+                                            <input type="text" class="form-control" id="phonenumberInput"
+                                                placeholder="Enter your phone number" value="+(1) 987 6543">
+                                        </div>
+                                    </div>
+                                    <!--end col-->
+                                    <div class="col-lg-6">
+                                        <div class="mb-3">
+                                            <label for="emailInput" class="form-label">Email
+                                                Address</label>
+                                            <input type="email" class="form-control" id="emailInput"
+                                                placeholder="Enter your email" value="daveadame@velzon.com">
+                                        </div>
+                                    </div>
+                                    <!--end col-->
+                                    <div class="col-lg-12">
+                                        <div class="hstack gap-2 justify-content-end">
+                                            <button type="submit" class="btn btn-primary">Updates</button>
+                                            <button type="button" class="btn btn-soft-success">Cancel</button>
+                                        </div>
+                                    </div>
+                                    <!--end col-->
+                                </div>
+                                <!--end row-->
+                            </form>
+                        </div>
+                        <!--end tab-pane-->
+                        <div class="tab-pane" id="changePassword" role="tabpanel">
+                            <form action="javascript:void(0);">
+                                <div class="row g-2">
+                                    <div class="col-lg-4">
+                                        <div>
+                                            <label for="oldpasswordInput" class="form-label">Old
+                                                Password*</label>
+                                            <input type="password" class="form-control" id="oldpasswordInput"
+                                                placeholder="Enter current password">
+                                        </div>
+                                    </div>
+                                    <!--end col-->
+                                    <div class="col-lg-4">
+                                        <div>
+                                            <label for="newpasswordInput" class="form-label">New
+                                                Password*</label>
+                                            <input type="password" class="form-control" id="newpasswordInput"
+                                                placeholder="Enter new password">
+                                        </div>
+                                    </div>
+                                    <!--end col-->
+                                    <div class="col-lg-4">
+                                        <div>
+                                            <label for="confirmpasswordInput" class="form-label">Confirm
+                                                Password*</label>
+                                            <input type="password" class="form-control" id="confirmpasswordInput"
+                                                placeholder="Confirm password">
+                                        </div>
+                                    </div>
+                                    <!--end col-->
+                                    <div class="col-lg-12">
+                                        <div class="mb-3">
+                                            <a href="javascript:void(0);"
+                                                class="link-primary text-decoration-underline">Forgot
+                                                Password ?</a>
+                                        </div>
+                                    </div>
+                                    <!--end col-->
+                                    <div class="col-lg-12">
+                                        <div class="text-end">
+                                            <button type="submit" class="btn btn-success">Change
+                                                Password</button>
+                                        </div>
+                                    </div>
+                                    <!--end col-->
+                                </div>
+                                <!--end row-->
+                            </form>
+
+                        </div>
+                        <!--end tab-pane-->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--end col-->
+    </div>
+    <!--end row-->
+@endsection
+@section('script')
+    <script src="{{ URL::asset('build/js/pages/profile-setting.init.js') }}"></script>
+    <script src="{{ URL::asset('build/js/app.js') }}"></script>
+@endsection
---
+++ resources/views/pages-profile.blade.php
@@ -0,0 +1,116 @@
+@extends('layouts.master')
+@section('title')
+    @lang('translation.profile')
+@endsection
+@section('css')
+    <link rel="stylesheet" href="{{ URL::asset('build/libs/swiper/swiper-bundle.min.css') }}">
+@endsection
+@section('content')
+    <div class="profile-foreground position-relative mx-n4 mt-n4">
+        <div class="profile-wid-bg">
+            <img src="{{ URL::asset('build/images/profile-bg.jpg') }}" alt="" class="profile-wid-img" />
+        </div>
+    </div>
+
+    <div class="pt-4 mb-4 mb-lg-3 pb-lg-4 profile-wrapper">
+        <div class="row g-4">
+            <div class="col-auto">
+                <div class="avatar-lg"
+                    style="  width: 50px;
+        height: 50px;
+        background-color: #3498db;
+        color: white;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 24px;
+        font-weight: bold;
+        border-radius: 50%;">
+                    <span class="avatar-text">A</span>
+                </div>
+            </div>
+            <!--end col-->
+            <div class="col">
+                <div class="p-2">
+                    <h3 class="text-white mb-1">Anna Adame</h3>
+                    <p class="text-white text-opacity-75">Owner & Founder</p>
+                    <div class="hstack text-white-50 gap-1">
+                        <div class="me-2"><i
+                                class="ri-map-pin-user-line me-1 text-white text-opacity-75 fs-16 align-middle"></i>California,
+                            United States</div>
+                    </div>
+                </div>
+            </div>
+            <!--end col-->
+            <div class="col-12 col-lg-auto order-last order-lg-0">
+
+            </div>
+            <!--end col-->
+
+        </div>
+        <!--end row-->
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div>
+                <div class="d-flex profile-wrapper">
+                    <!-- Nav tabs -->
+                    <ul class="nav nav-pills animation-nav profile-nav gap-2 gap-lg-3 flex-grow-1" role="tablist">
+
+                    </ul>
+                    <div class="flex-shrink-0">
+                        <a href="pages-profile-settings" class="btn btn-success"><i
+                                class="ri-edit-box-line align-bottom"></i> Edit Profile</a>
+                    </div>
+                </div>
+                <!-- Tab panes -->
+                <div class="tab-content pt-4 text-muted">
+                    <div class="tab-pane active" id="overview-tab" role="tabpanel">
+                        <div class="row">
+                            <div class="col-xxl-3">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title mb-3">Profile Details</h5>
+                                        <div class="table-responsive">
+                                            <table class="table table-borderless mb-0">
+                                                <tbody>
+                                                    <tr>
+                                                        <th class="ps-0" scope="row">Full Name :</th>
+                                                        <td class="text-muted">Anna Adame</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th class="ps-0" scope="row">Mobile :</th>
+                                                        <td class="text-muted">+(1) 987 6543</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th class="ps-0" scope="row">E-mail :</th>
+                                                        <td class="text-muted">daveadame@velzon.com</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th class="ps-0" scope="row">Location :</th>
+                                                        <td class="text-muted">California, United States
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!--end col-->
+    </div>
+    <!--end row-->
+@endsection
+@section('script')
+    <script src="{{ URL::asset('build/libs/swiper/swiper-bundle.min.js') }}"></script>
+
+    <script src="{{ URL::asset('build/js/pages/profile.init.js') }}"></script>
+    <script src="{{ URL::asset('build/js/app.js') }}"></script>
+@endsection
