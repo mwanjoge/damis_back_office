39c5b25c6ab8562e85959fe344c11852d72c3a2c
mwanjoge <boss.sm11@gmail.com>
2025-05-02 01:25:55 +0300

    update layoute

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -42,15 +42,10 @@
     <!-- JAVASCRIPT -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     @include('layouts.vendor-scripts')
-
-    
-
-<!--select2 cdn-->
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-
-<script src="{{ URL::asset('build/js/pages/select2.init.js') }}"></script>
-@yield('script')
-
+    <!--select2 cdn-->
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script src="{{ URL::asset('build/js/pages/select2.init.js') }}"></script>
+    @yield('script')
 </body>
 
 </html>
