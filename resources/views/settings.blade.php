1dd15f785c07f9378c9071074c0791857e0ae9a9
mwanjoge <boss.sm11@gmail.com>
2025-05-02 00:35:38 +0300

    on setting page make current tab stick

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -56,3 +56,21 @@
         </div>
     </div>
 @endsection
+@section('script')
+    <script>
+        $(function () {
+            // for bootstrap 3 use 'shown.bs.tab', for bootstrap 2 use 'shown' in the next line
+            $('a[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
+                // save the latest tab; use cookies if you like 'em better:
+                localStorage.setItem('lastTab', $(this).attr('href'));
+            });
+
+            // go to the latest tab, if it exists:
+            var lastTab = localStorage.getItem('lastTab');
+            if (lastTab) {
+                $('[href="' + lastTab + '"]').tab('show');
+            }
+        });
+
+    </script>
+@endsection
