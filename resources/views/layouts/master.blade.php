285bd7fbadc283dec7071d9371e50ce97bdd91c4
mwanjoge <boss.sm11@gmail.com>
2025-05-02 01:26:16 +0300

    update layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -37,7 +37,7 @@
     </div>
     <!-- END layout-wrapper -->
 
-    @include('layouts.customizer')
+    {{-- @include('layouts.customizer') --}}
 
     <!-- JAVASCRIPT -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
