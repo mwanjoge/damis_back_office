7f16cd7ed538afc9486057d6af1a091dad1889b9
mwanjoge <boss.sm11@gmail.com>
2025-05-21 10:21:55 +0300

    update login page

--- resources/views/layouts/tabler/auth.blade.php
+++ resources/views/layouts/tabler/auth.blade.php
@@ -42,40 +42,7 @@
 
                 <!-- END NAVBAR LOGO -->
                 <div class="navbar-nav flex-row order-md-last">
-                    <a href="#" class="nav-link d-flex lh-1 p-0 px-2 position-relative" data-bs-toggle="dropdown"
-                        aria-label="Open user menu">
-                        <!-- Bell SVG Icon -->
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round" class="icon icon-1 text-white  me-1">
-                            <path
-                                d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3H4a4 4 0 0 0 2-3v-3a7 7 0 0 1 4-6" />
-                            <path d="M9 17v1a3 3 0 0 0 6 0v-1" />
-                        </svg>
-                    </a>
-
                     <div class="nav-item dropdown">
-
-                        <a href="#" class="nav-link d-flex lh-1 p-0 px-2" data-bs-toggle="dropdown"
-                            aria-label="Open user menu">
-                            <span class="avatar avatar-sm"
-                                style="background-image: url({{ URL::asset('build/images/users/user-avatar.jpeg') }})">
-                            </span>
-                            <div class="d-none d-xl-block ps-2">
-                                @auth
-                                    <div>
-                                        <span
-                                            class="d-none d-xl-inline-block ms-1 fw-medium user-name-text text-white">{{ Auth::user()->name }}</span>
-                                        </span>
-                                    </div>
-                                    <div class="mt-1 small text-secondary">
-                                        <span
-                                            class="d-none d-xl-inline-block ms-1 fw-medium user-name-text text-white">{{ Auth::user()->role }}</span>
-                                        </span>
-                                    </div>
-                                @endauth
-                            </div>
-                        </a>
                         <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
 
                             <a class="dropdown-item" href="{{ route('user-profile') }}"><i
