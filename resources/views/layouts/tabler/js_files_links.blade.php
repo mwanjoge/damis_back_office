bf44dec9f25f1d98ca558480f285c341483b015a
mwanjoge <boss.sm11@gmail.com>
2025-05-21 11:04:58 +0300

    update datatable style

--- resources/views/layouts/tabler/js_files_links.blade.php
+++ resources/views/layouts/tabler/js_files_links.blade.php
@@ -10,9 +10,14 @@
 
 <!-- DataTables -->
 <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
-<script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.datatables.net/2.3.1/js/dataTables.js"></script>
+<script src="https://cdn.datatables.net/2.3.1/js/dataTables.bootstrap5.js"></script>
+
+{{-- https://code.jquery.com/jquery-3.7.1.js
+https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/js/bootstrap.bundle.min.js
+https://cdn.datatables.net/2.3.1/js/dataTables.js
+https://cdn.datatables.net/2.3.1/js/dataTables.bootstrap5.js --}}
 <script src="{{ asset('vendors/tabler/js/app.js') }}"></script>
 
 <!-- Chart.js -->
@@ -37,4 +42,4 @@
 {{-- <script src="{{ asset('vendors/tabler/js/demo.min.js') }}" defer></script> --}}
 <!-- END DEMO SCRIPTS -->
 <!-- BEGIN PAGE SCRIPTS -->
-<script src="//cdnjs.cloudflare.com/ajax/libs/list.js/1.5.0/list.min.js"></script>
+
