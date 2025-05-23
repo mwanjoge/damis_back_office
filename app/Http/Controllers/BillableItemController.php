c4c9b422f1b477b82446d810a9532b18eca5b80c
john <johnjr092@gmail.com>
2025-05-23 18:13:01 +0300

    permissions update

--- app/Http/Controllers/BillableItemController.php
+++ app/Http/Controllers/BillableItemController.php
@@ -16,9 +16,9 @@ class BillableItemController extends Controller
      public function __construct()
     {
         $model = 'billable_item';
-        $this->middleware("permission:view_{$model}")->only(['index', 'show']);
+        $this->middleware("permission:read_{$model}")->only(['index', 'show']);
         $this->middleware("permission:create_{$model}")->only(['create', 'store']);
-        $this->middleware("permission:edit_{$model}")->only(['edit', 'update']);
+        $this->middleware("permission:update_{$model}")->only(['edit', 'update']);
         $this->middleware("permission:delete_{$model}")->only(['destroy']);
     }
     public function index()
