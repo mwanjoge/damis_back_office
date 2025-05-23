179a2747a13081f5421bfe828c21b9622f945a31
mwanjoge <boss.sm11@gmail.com>
2025-05-23 19:06:56 +0300

    bug fix

--- resources/views/emails/invoice.blade.php
+++ resources/views/emails/invoice.blade.php
@@ -1,7 +1,7 @@
 <div style="align: center;">
    <img src="{{ asset('images/emblem.png') }}" alt="Logo" style="width: 100px; height: auto;" />
     <h1 style="text-align: center; background-color:#eee">Invoice {{ $invoice->ref}}</h1>
-    <p style="text-align: center;">Hello {{ $invoice->member->name}} your invoice has been created please pay to continue with you request</p>
+    <p style="text-align: center;">Hello {{ $invoice->member?->name}} your invoice has been created please pay to continue with you request</p>
     <h2>INVOICE DETAILS</h2>
     <table style="width: 100%; border-collapse: collapse; margin-bottom: 20px;">
         <thead>
@@ -14,7 +14,7 @@
         <tbody>
             @foreach ($invoice->generalLineItems as $item)
                 <tr>
-                    <td style="border: 1px solid #ddd; padding: 8px;">{{ $item->service->name }}</td>
+                    <td style="border: 1px solid #ddd; padding: 8px;">{{ $item->service?->name }}</td>
                     {{-- <td style="border: 1px solid #ddd; padding: 8px;">{{ $item->description }}</td> --}}
                     <td style="border: 1px solid #ddd; padding: 8px;">{{ $item->price }}</td>
                 </tr>
